860d1823382dfa7dacc71084d6dcbf179192f61f
Loy van Beek <l.vanbeek@ultimaker.com>
2016-06-17 14:46:20 +0200

    Use unittest assertEquals rather than plain asserts. This gives clearer feedback

--- nfc/ndef/tests/ndef_message_test.py
+++ nfc/ndef/tests/ndef_message_test.py
@@ -33,61 +33,61 @@ class TestMessageInit(unittest.TestCase):
 
     def test_init_args_none(self):
         message = Message()
-        assert isinstance(message, Message)
+        self.assertIsInstance(message, Message)
 
     def test_init_args_bytestr(self):
         message = Message(b"\xD0\x00\x00")
-        assert len(message) == 1
+        self.assertEquals(len(message), 1)
 
     def test_init_args_bytearray(self):
         message = Message(bytearray(b"\xD0\x00\x00"))
-        assert len(message) == 1
+        self.assertEquals(len(message), 1)
 
     def test_init_args_bytestream(self):
         message = Message(io.BytesIO(b"\xD0\x00\x00"))
-        assert len(message) == 1
+        self.assertEquals(len(message), 1)
 
     def test_generate_bytestr(self):
         message = Message(b"\xD0\x00\x00")
-        assert message.to_bytes() == b"\xD0\x00\x00"
+        self.assertEquals(message.to_bytes(), b"\xD0\x00\x00")
 
     def test_init_args_one_record(self):
         record = Record()
         message = Message(record)
-        assert message.to_bytes() == b"\xD0\x00\x00"
+        self.assertEquals(message.to_bytes(), b"\xD0\x00\x00")
 
     def test_init_args_two_records(self):
         record = Record()
         message = Message(record, record)
-        assert message.to_bytes() == b"\x90\x00\x00\x50\x00\x00"
+        self.assertEquals(message.to_bytes(), b"\x90\x00\x00\x50\x00\x00")
 
 class TestMessageMethods(unittest.TestCase):
     def test_method_length(self):
         message = Message()
-        assert len(message) == 0
+        self.assertEquals(len(message), 0)
 
     def test_method_getitem(self):
         message = Message()
-        try: assert message[0]
+        try: assert (message[0])
         except IndexError: pass
 
     def test_method_append(self):
         message = Message()
         message.append(Record())
-        assert len(message) == 1
-        assert isinstance(message[0], Record)
+        self.assertEquals(len(message), 1)
+        self.assertIsInstance(message[0], Record)
 
     def test_method_extend(self):
         message = Message()
         message.extend([Record()])
-        assert len(message) == 1
-        assert isinstance(message[0], Record)
+        self.assertEquals(len(message), 1)
+        self.assertIsInstance(message[0], Record)
 
     def test_method_insert(self):
         message = Message()
         message.insert(0, Record())
-        assert len(message) == 1
-        assert isinstance(message[0], Record)
+        self.assertEquals(len(message), 1)
+        self.assertIsInstance(message[0], Record)
 
 class TestMessageFailure(unittest.TestCase):
 
