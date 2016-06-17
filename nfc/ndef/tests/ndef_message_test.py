97e0f0fea8d50446c396d15d0f384b675e5818f5
Loy van Beek <l.vanbeek@ultimaker.com>
2016-06-17 14:19:14 +0200

    Convert tests for Message to unittest

--- nfc/ndef/tests/ndef_message_test.py
+++ nfc/ndef/tests/ndef_message_test.py
@@ -20,73 +20,78 @@
 # See the Licence for the specific language governing
 # permissions and limitations under the Licence.
 # -----------------------------------------------------------------------------
-import sys, os
-sys.path.insert(1, os.path.split(sys.path[0])[0])
+
+import unittest
 
 import io
-import nfc.ndef
 
-def test_init_args_none():
-    message = nfc.ndef.Message()
-    assert isinstance(message, nfc.ndef.Message)
+from ..record import Record
+from ..message import Message
+from ..error import FormatError, LengthError
 
-def test_method_length():
-    message = nfc.ndef.Message()
-    assert len(message) == 0
+class TestMessage(unittest.TestCase):
 
-def test_method_getitem():
-    message = nfc.ndef.Message()
-    try: assert message[0]
-    except IndexError: pass
+    def test_init_args_none(self):
+        message = Message()
+        assert isinstance(message, Message)
 
-def test_method_append():
-    message = nfc.ndef.Message()
-    message.append(nfc.ndef.Record())
-    assert len(message) == 1
-    assert isinstance(message[0], nfc.ndef.Record)
-    
-def test_method_extend():
-    message = nfc.ndef.Message()
-    message.extend([nfc.ndef.Record()])
-    assert len(message) == 1
-    assert isinstance(message[0], nfc.ndef.Record)
+    def test_method_length(self):
+        message = Message()
+        assert len(message) == 0
 
-def test_method_insert():
-    message = nfc.ndef.Message()
-    message.insert(0, nfc.ndef.Record())
-    assert len(message) == 1
-    assert isinstance(message[0], nfc.ndef.Record)
+    def test_method_getitem(self):
+        message = Message()
+        try: assert message[0]
+        except IndexError: pass
 
-def test_init_args_bytestr():
-    message = nfc.ndef.Message(b"\xD0\x00\x00")
-    assert len(message) == 1
-    
-def test_init_args_bytearray():
-    message = nfc.ndef.Message(bytearray("\xD0\x00\x00"))
-    assert len(message) == 1
+    def test_method_append(self):
+        message = Message()
+        message.append(Record())
+        assert len(message) == 1
+        assert isinstance(message[0], Record)
 
-def test_init_args_bytestream():
-    message = nfc.ndef.Message(io.BytesIO(b"\xD0\x00\x00"))
-    assert len(message) == 1
+    def test_method_extend(self):
+        message = Message()
+        message.extend([Record()])
+        assert len(message) == 1
+        assert isinstance(message[0], Record)
 
-def test_generate_bytestr():
-    message = nfc.ndef.Message(b"\xD0\x00\x00")
-    assert str(message) == b"\xD0\x00\x00"
+    def test_method_insert(self):
+        message = Message()
+        message.insert(0, Record())
+        assert len(message) == 1
+        assert isinstance(message[0], Record)
 
-def test_init_args_one_record():
-    record = nfc.ndef.Record()
-    message = nfc.ndef.Message(record)
-    assert str(message) == b"\xD0\x00\x00"
-    
-def test_init_args_two_records():
-    record = nfc.ndef.Record()
-    message = nfc.ndef.Message(record, record)
-    assert str(message) == b"\x90\x00\x00\x50\x00\x00"
-    
-def test_failure_mb_not_set():
-    try: message = nfc.ndef.Message(b"\x10\x00\x00")
-    except nfc.ndef.FormatError: pass
+    def test_init_args_bytestr(self):
+        message = Message(b"\xD0\x00\x00")
+        assert len(message) == 1
 
-def test_failure_length_error():
-    try: message = nfc.ndef.Message(b"\x10\x01\x00")
-    except nfc.ndef.LengthError: pass
+    def test_init_args_bytearray(self):
+        message = Message(bytearray("\xD0\x00\x00"))
+        assert len(message) == 1
+
+    def test_init_args_bytestream(self):
+        message = Message(io.BytesIO(b"\xD0\x00\x00"))
+        assert len(message) == 1
+
+    def test_generate_bytestr(self):
+        message = Message(b"\xD0\x00\x00")
+        assert str(message) == b"\xD0\x00\x00"
+
+    def test_init_args_one_record(self):
+        record = Record()
+        message = Message(record)
+        assert str(message) == b"\xD0\x00\x00"
+
+    def test_init_args_two_records(self):
+        record = Record()
+        message = Message(record, record)
+        assert str(message) == b"\x90\x00\x00\x50\x00\x00"
+
+    def test_failure_mb_not_set(self):
+        try: message = Message(b"\x10\x00\x00")
+        except FormatError: pass
+
+    def test_failure_length_error(self):
+        try: message = Message(b"\x10\x01\x00")
+        except LengthError: pass
